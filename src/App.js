77ad41582f0255cde6a606417f5ff242706219f4
Ali Jahankah <aral2@yahoo.com>
2024-11-06 18:11:28 +0000

    learn three.js. creating and animating a cube

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import * as Three from 'three';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { useEffect, useRef } from 'react';
+const App = () => {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const scene = new Three.Scene();
+    const boxMaterials = [
+      new Three.MeshBasicMaterial({ color: '#faaf18' }),
+      new Three.MeshBasicMaterial({ color: '#18fa20' }),
+      new Three.MeshBasicMaterial({ color: '#18faf6' }),
+      new Three.MeshBasicMaterial({ color: '#f576ad' }),
+      new Three.MeshBasicMaterial({ color: '#e01017' }),
+      new Three.MeshBasicMaterial({ color: '#1876fa' })
+    ];
+    const box = new Three.BoxGeometry(1, 1, 1);
+    new Three.MeshBasicMaterial({ color: 'red' });
+    const mesh = new Three.Mesh(box, boxMaterials);
+    scene.add(mesh);
+    const aspect = {
+      width: window.innerWidth,
+      height: window.innerHeight
+    };
+    const camera = new Three.PerspectiveCamera(
+      75,
+      aspect.width / aspect.height
+    );
+    camera.position.z = 3;
+    scene.add(camera);
+    const rerenderer = new Three.WebGLRenderer({ canvas: canvasRef.current });
+    rerenderer.setSize(aspect.width, aspect.height);
+
+    const animate = () => {
+      window.requestAnimationFrame(animate);
+      mesh.rotation.x += 0.01;
+      mesh.rotation.y += 0.01;
+
+      rerenderer.render(scene, camera);
+    };
+    animate();
+    return () => {
+      rerenderer.dispose();
+    };
+  }, []);
+
+  return <canvas ref={canvasRef}></canvas>;
+};
 
 export default App;
