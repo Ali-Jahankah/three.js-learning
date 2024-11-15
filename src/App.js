9fbc747aa5b44f0d21165e7dd47ed5d431b81c92
Ali Jahankah <aral2@yahoo.com>
2024-11-15 18:11:55 +0000

    add music

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import * as contentful from 'contentful';
-import React, { useMemo, useState, Suspense } from 'react';
-
-import { useEffect } from 'react';
+import React, { useMemo, useState, Suspense, useEffect, useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, useGLTF, useProgress, Html } from '@react-three/drei';
+import {
+  OrbitControls,
+  useGLTF,
+  useProgress,
+  Html,
+  PositionalAudio
+} from '@react-three/drei';
 
 const App = () => {
-  const [, setData] = useState([]);
-
+  const [data, setData] = useState([]);
   const client = useMemo(() => {
     return contentful.createClient({
       space: process.env.REACT_APP_SPACE,
@@ -20,35 +23,60 @@ const App = () => {
     const fetchData = async () => {
       try {
         const res = await client.getEntries();
+        console.log(res.items);
         setData(res.items);
       } catch (error) {
         console.log(error);
       }
     };
     fetchData();
-    // eslint-disable-next-line
   }, [client]);
+
+  const audioRef = useRef(null);
 
   function Model() {
     const { scene } = useGLTF('./gaming_room/scene.gltf', true);
-    return <primitive object={scene} position={[0, -4, 0]} />;
+    return (
+      <primitive
+        object={scene}
+        position={[0, -4, 0]}
+        onClick={(e) => {
+          console.log(e.intersections[0].object.name);
+          if (e.intersections[0].object.name === 'Object_24') {
+            if (audioRef.current) {
+              audioRef.current.isPlaying
+                ? audioRef.current.pause()
+                : audioRef.current.play();
+            }
+          }
+        }}
+      />
+    );
   }
 
   function Loader() {
-    console.log('loading');
     const { progress } = useProgress();
     return (
-      <Html center>
-        <div style={{ color: 'white', fontSize: '2rem' }}>
-          Loading... {Math.round(progress)}%
-        </div>
+      <Html
+        as="div"
+        center={true}
+        style={{
+          justifyContent: 'center',
+          color: 'white',
+          fontSize: '2rem',
+          textAlign: 'center',
+          width: '400px'
+        }}
+      >
+        <div>Loading 3D model and music... {Math.round(progress)}%</div>
       </Html>
     );
   }
+
   return (
     <div className="model">
       <Canvas camera={{ position: [8, 3, 0], near: 0.01, far: 1000 }}>
-        <Suspense fallback={<Loader></Loader>}>
+        <Suspense fallback={<Loader />}>
           <ambientLight intensity={1} color="#ffffff" />
           <directionalLight
             intensity={1}
@@ -65,7 +93,18 @@ const App = () => {
             position={[10, 0, 1.3]}
             color="#e059c5"
           />
+
           <Model />
+
+          {data.length > 0 && data[0].fields.music && (
+            <PositionalAudio
+              url={data[0].fields.music.fields.file.url}
+              ref={audioRef}
+              loop
+              distance={0.1}
+            />
+          )}
+
           <OrbitControls
             target={[0, 2, 0]}
             enablePan={false}
